bcd0288785f0e88dc915149fc8b8d85631a3cac7
Cristhian Pineda <cpineda@mobilges.com>
2020-10-01 08:43:04 -0600

    Comit inicial

---
+++ src/app/components/ng-style/ng-style.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ng-style',
+  template: `
+  <p [style.fontSize.px]="tamano">hola mundo... esta es una etiqueta html</p>
+  <button class="btn btn-primary btn-space" (click)="tamano = tamano + 5">
+    <i class="fa fa-plus"></i>
+  </button>
+  <button class="btn btn-danger" (click)="tamano = tamano - 5">
+    <i class="fa fa-minus"></i>
+  </button>
+  `,
+  styles: [
+  ]
+})
+export class NgStyleComponent implements OnInit {
+
+  tamano = 30;
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
